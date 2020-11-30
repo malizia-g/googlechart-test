a1670937214fe48bcb47ce61d51aaca4ec23d47a
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2020-11-30 11:45:16 +0000

    primo esempio

---
+++ google-charts/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ChartType } from 'angular-google-charts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Browser market shares at a specific website, 2014';
+  type  = ChartType.BarChart;
+  data = [
+    ['Firefox', 45.0],
+    ['IE', 26.8],
+    ['Chrome', 12.8],
+    ['Safari', 8.5],
+    ['Opera', 6.2],
+    ['Others', 0.7]
+  ];
+  columnNames = ['Browser', 'Percentage'];
+  options = {
+  };
+  width = 550;
+  height = 400;
+
+}
---
+++ google-charts/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GoogleChartsModule } from 'angular-google-charts';;
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    GoogleChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
